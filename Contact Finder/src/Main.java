c3b3c20ee30ba3b469efc1249d7ef2e4d916426e
J.L. Vargas <78johnlloyd@gmail.com>
2025-10-21 22:04:22 +0800

    Revert "Add statistics"
    This reverts commit 071257a73f5c37b8121469fa4b97a45cb5fc2084.

--- Contact Finder/src/Main.java
+++ Contact Finder/src/Main.java
@@ -8,8 +8,6 @@ public class Main {
     private BufferedReader br;
     private Scanner in = new Scanner(System.in);
     private ArrayList<ArrayList<String>> contactRecords = new ArrayList<>();
-    private ArrayList<Statistics> statList = new ArrayList<>();
-    private long TIME_RESULT;
 
     public Main() {
         try {
@@ -39,7 +37,6 @@ public class Main {
                     searchByName();
                     break;
                 case 3:
-                    showStats();
                     break;
                 case 4:
                     return;
@@ -62,59 +59,22 @@ public class Main {
         ArrayList<ArrayList<String>> findName = linearSearch(name);
         System.out.println("\nFound " + (findName.size() - 1) + "x record(s) for name " + name);
         System.out.println("-".repeat(50));
-        for (int i = 0; i < findName.size(); i++) {
+        for (int i = 0; i < findName.size() - 1; i++) {
             System.out.printf("%-5s %-5s | %-5s%n", (i + 1) + ".", findName.get(i).get(0), findName.get(i).get(1));
         }
     }
 
-    private void showStats() {
-        while (true) {
-            System.out.println("=".repeat(10) + " STATISTICS MENU " + "=".repeat(10));
-            System.out.println("[0] Go Back");
-            System.out.println("[1] Search by ID stats (Binary Search)");
-            System.out.println("[2] Search by Name stats (Linear Search)");
-            System.out.print("Enter choice >>: ");
-            int userChoice = Integer.parseInt(in.nextLine());
-            switch (userChoice) {
-                case 0:
-                    return;
-                case 1:
-                    System.out.println("\n" + "=".repeat(10) + " STATISTICS " + "=".repeat(10));
-                    for (Statistics stats : statList) {
-                        if (stats.getSearchType().equals("Binary")) {
-                            stats.summarizeStats(TIME_RESULT);
-                        }
-                    }
-                    break;
-                case 2:
-                    System.out.println("\n" + "=".repeat(10) + " STATISTICS " + "=".repeat(10));
-                    for (Statistics stats : statList) {
-                        if (stats.getSearchType().equals("Linear")) {
-                            stats.summarizeStats();
-                        }
-                    }
-                    break;
-            }
-        }
-    }
-
     private ArrayList<ArrayList<String>> binarySearch(int key) {
-        ArrayList<Long> timeArray = new ArrayList<>();
-        ArrayList<ArrayList<String>> result = new ArrayList<>();
         int left = 0;
         int right = contactRecords.size() - 1;
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
 
-        long TIME_START, TIME_END;
-        TIME_START = System.nanoTime();
         while (left <= right) {
             int middle = left + (right - left) / 2;
             int middleId = Integer.parseInt(contactRecords.get(middle).getFirst());
 
             if (middleId == key) {
                 result.add(contactRecords.get(middle));
-                TIME_END = System.nanoTime();
-                timeArray.add(TIME_END - TIME_START);
-                statList.add(new Statistics(timeArray, "Binary", key + ""));
                 return result;
             }
             if (middleId < key) {
@@ -123,34 +83,20 @@ public class Main {
                 right = middle - 1;
             }
         }
-        TIME_END = System.nanoTime();
-        timeArray.add(TIME_END - TIME_START);
-        statList.add(new Statistics(timeArray, "Binary", key + ""));
-        return result;
+        return null;
     }
 
     private ArrayList<ArrayList<String>> linearSearch(String key) {
-        ArrayList<Long> timeArray = new ArrayList<>();
         ArrayList<ArrayList<String>> names = new ArrayList<>();
-        long TIME_START, TIME_END;
-
         for (ArrayList<String> row : contactRecords) {
-            TIME_START = System.nanoTime();
-            boolean match = row.get(1).contains(key);
-            TIME_END = System.nanoTime();
-            timeArray.add(TIME_END - TIME_START);
-
-            if (match) {
+            if (row.get(1).contains(key)) {
                 names.add(row);
             }
         }
-        statList.add(new Statistics(timeArray, "Linear", key));
         return names;
     }
 
     private void insertionSort() {
-        long START = System.nanoTime();
-
         for (int i = 1; i < contactRecords.size(); i++) {
             ArrayList<String> currentRow = contactRecords.get(i);
             int currentValue = Integer.parseInt(currentRow.get(0));
@@ -162,8 +108,6 @@ public class Main {
             }
             contactRecords.set(j + 1, currentRow);
         }
-        long END = System.nanoTime();
-        TIME_RESULT = END - START;
     }
 
     private void addToArray() {
